8c0a7447dfa0bd8719bbf5a105b00f64abdcdd02
KRISHNAJHA010 <krishna.brahmachari.15@gmail.com>
2025-06-02 13:47:19 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,9 +9,9 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 
 # --- CONFIG ---
-API_ID = 21737663  # Replace with your API ID
-API_HASH = "1898391366167db54389f40cb6f37243"
-BOT_TOKEN = "8074621768:AAGcANPuhUJIq1_l3Nu-bfiPIXKrFkmZl9k"
+API_ID = 12345678  # Replace with your API ID
+API_HASH = "your_api_hash"
+BOT_TOKEN = "your_bot_token"
 RECENTS_FILE = "recents.json"
 PLAYLISTS_FILE = "playlists.json"
 
